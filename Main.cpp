8b2c8086f776eadf90afba1e4be1202eda5774e0
Andrey <zertsalov96@yandex.ru>
2014-12-21 20:47:23 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "tests.h"
 #include <iostream>
 #include <vector>
@@ -7,23 +9,13 @@ using namespace std;
 
 int main()
 {
-    clock_t t;
-    //Binomial_heap_array heaps;
-    //Leftist_heap_array heaps;
-    int n, x;
+    int n;
     cin >> n;
-    testing(n);
-    /*t = clock();
-    heaps.add_heap(0);
-    for (int i = 1; i < n; i++)
-    {
-        heaps.insert(0, i);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << heaps.extract_min(0) << endl;
-    }
-    cout << (double)(clock() - t) / CLOCKS_PER_SEC;*/
+	while (n)
+	{
+		testing(n);
+		cin >> n;
+	}
     system("pause");
     return 0;
 }
